5224347aaadb2a4f1ff5c19c26a9dcb0a3c90f0d
springGitUser <springGitUser@gmail.com>
2021-12-10 10:34:56 +0530

    Added security to Zuul gateway

---
+++ PhotoAppApiGateway/src/main/java/com/springcloud/photoapp/apigateway/security/AuthorizationFilter.java
@@ -0,0 +1,59 @@
+package com.springcloud.photoapp.apigateway.security;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import io.jsonwebtoken.Jwts;
+
+public class AuthorizationFilter extends BasicAuthenticationFilter {
+
+	private Environment env;
+
+	public AuthorizationFilter(AuthenticationManager authenticationManager, Environment env) {
+		super(authenticationManager);
+		this.env = env;
+	}
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+
+		String authHeader = request.getHeader("Authorization");
+
+		if (authHeader == null || !authHeader.startsWith("Bearer")) {
+			chain.doFilter(request, response);
+			return;
+		}
+
+		UsernamePasswordAuthenticationToken authentication = getAuthentication(request);
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		chain.doFilter(request, response);
+
+	}
+
+	private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request) {
+		String authHeader = request.getHeader("Authorization");
+		String token = authHeader.replace("Bearer", "");
+		System.out.println("Token from header==>" + token);
+		String userId = Jwts.parser().setSigningKey(env.getProperty("token.secret")).parseClaimsJws(token).getBody()
+				.getSubject();
+
+		if (userId == null) {
+			return null;
+		}
+
+		return new UsernamePasswordAuthenticationToken(userId, null, new ArrayList<>());
+	}
+}
---
+++ PhotoAppApiGateway/src/main/java/com/springcloud/photoapp/apigateway/security/WebSecurity.java
@@ -0,0 +1,35 @@
+package com.springcloud.photoapp.apigateway.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private Environment env;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.headers().frameOptions().disable();// For h2-console
+
+		http.authorizeRequests().antMatchers(env.getProperty("user.ws.login.uri")).permitAll()
+				.antMatchers(env.getProperty("user.ws.registration.uri")).permitAll()
+				.antMatchers(env.getProperty("user.ws.h2console.uri")).permitAll().anyRequest().authenticated()
+				.and().addFilter(new AuthorizationFilter(authenticationManager(), env));
+
+		// To Prevent http Autherization headers to be cached we make session
+		// STATLESS-->Spring security will never create a http session and obtain
+		// authentication object from SecurityContext...
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+	}
+
+}
